50df7ca55f5d37d3ae303985e17cf43bbcda7e0a
Bonuspunkt <bonuspunkt+git@gmail.com>
2013-11-06 19:57:57 +0100

    fix index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   ContentManager: require('./lib/contentManager'),
   DrawableGameComponent: require('./lib/drawableGameComponent'),
   EventEmitter: require('./lib/eventEmitter'),
-  Game: require('./lib/game'),
   GameComponent: require('./lib/gameComponent'),
   GameComponentCollection: require('./lib/gameComponentCollection'),
   util: require('./lib/util'),
